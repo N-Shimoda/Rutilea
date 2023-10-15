dcab88200ced5839085639482a4b406f9fb47188
N-Shimoda <naoki.shimoda@outlook.jp>
2023-10-15 16:27:47 +0900

    basic view middle

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Rutilea 選考課題2
+## About
 Developing an application that uses LLM to select music appropriate to the atmosphere of a given photograph.
+
+## Files & Directories
+- `main.py` : Toplevel of application.
+
+## Plan of Development
+1. 単一画像でのビュー完成
+1. 複数画像を選べるようにする
+1. LLM を使ったバックエンドの作成
+
+## Todo
+- 音楽の表示方法の検討
 
 ## Links
 - [Tasks for Non-AI-native Application](https://docs.google.com/document/d/1BQRDnBziHUUKzwzlD68ZyD3Bq0XrTHt2dYSwgHeLGP8/edit)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import customtkinter as ctk
+from PIL import Image
 
 class App(ctk.CTk):
 
@@ -9,19 +10,34 @@ class App(ctk.CTk):
         self.geometry("300x360")
         self.title("Scenery sommelier")
 
-        self.window = ctk.CTkFrame(self, bg_color="white")
-        self.window.pack(expand=True)
+        # ---- Frame ----
+        self.frame_main = ctk.CTkFrame(self, bg_color="white")
+        self.frame_main.pack(expand=True)
 
-        self.upload_button = ctk.CTkButton(self.window, text="Select image", command=self.upload_image)
+        # ---- Widgets ----
+        # button
+        self.upload_button = ctk.CTkButton(self.frame_main, text="Select image", command=self.upload_image)
         self.upload_button.pack()
 
+        # image
+        self.img = Image.open("img/suits_dining_scene.jpg")
+        my_image = ctk.CTkImage(
+            light_image=self.img,
+            # size=(self.winfo_screenheight, self.winfo_screenwidth)
+            size = tuple(item * 0.2 for item in self.img.size)
+        )
+        image_label = ctk.CTkLabel(self, image=my_image, text="")  # display image with a CTkLabel
+        image_label.pack()
+
     def upload_image(self):
-        file_path = ctk.filedialog.askopenfilename(filetypes=[("画像ファイル","*.jpg")]) 
+        file_path = ctk.filedialog.askopenfilename(filetypes=[("画像ファイル", "*.jpg")]) 
         print(file_path)
+
+    
 
 
 if __name__ == "__main__":
 
-    # ctk.set_appearance_mode("Dark")
+    ctk.set_appearance_mode("Dark")
     app = App()
     app.mainloop()
