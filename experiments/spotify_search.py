cb690cac4ab26651080340e75136a1b1e2bfb7c5
N-Shimoda <naoki.shimoda@outlook.jp>
2023-10-16 16:35:05 +0900

    sporify exp finished

---
+++ experiments/spotify_search.py
@@ -0,0 +1,38 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+import os
+import json
+from utility import colorize
+
+# 環境変数にClient IDとClient Secretを設定する
+os.environ["SPOTIPY_CLIENT_ID"] = "295130f2a4764bc9a423387a20a3d84c"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "a2e47ef747e24bc68809909c2105efda"
+
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
+if len(sys.argv) > 1:
+    name = ' '.join(sys.argv[1:])
+else:
+    name = '勇者'
+
+results = spotify.search(q='track:'+name, type='track')
+# target_dict = results["tracks"]
+
+# print(target_dict)
+# print(colorize(type(target_dict), 41))
+
+# print(results)
+# items = results['artists']['items']
+# if len(items) > 0:
+#     artist = items[0]
+#     print(artist['name'], artist['images'][0]['url'])
+
+# pretty_dict = json.dumps(target_dict, indent=4, sort_keys=True)
+# print(pretty_dict)
+
+url = results["tracks"]["items"][0]["external_urls"]["spotify"]
+preview_url = results["tracks"]["items"][0]["preview_url"]
+
+print("track URL: {}".format(url))
+print("preview: {}".format(preview_url))
--- experiments/utility.py
+++ experiments/utility.py
@@ -1,2 +1,3 @@
+# Reference: https://www.python.ambitious-engineer.com/archives/3721
 def colorize(text, color_code):
     return f"\033[{color_code}m{text}\033[0m"
