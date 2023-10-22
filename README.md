398bda857417fb668f9dd04a6161740e45462888
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2023-10-22 14:24:09 +0900

    image-to-music middle

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 ## About
 Developing an application that uses LLM to select music appropriate to the atmosphere of a given photograph.
 
+## Usage
+Run following command in your terminal. Program should be ran in top directory (`Rutilea`).
+> python main.py
+
 ## Files & Directories
 - `main.py` : Toplevel of application.
+- `src`
+    + `spotify.py`
+    + `visual_LLM.py`
+- `img`
+- `experiments`
 - `environment.yml` : Requirements for conda environment.
-
-## Plan of Development
-1. 単一画像でのビュー完成
-1. 複数画像を選べるようにする
-1. LLM を使ったバックエンドの作成
-
-## Todo
-- 音楽の表示方法の検討
 
 ## Links
 - [Tasks for Non-AI-native Application](https://docs.google.com/document/d/1BQRDnBziHUUKzwzlD68ZyD3Bq0XrTHt2dYSwgHeLGP8/edit)
--- src/visual_LLM.py
+++ src/visual_LLM.py
@@ -1,5 +1,53 @@
-def image_to_text(image) -> str:
+import os
+import openai
+from tempfile import NamedTemporaryFile
+from langchain.agents import initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from image_tools import ImageCaptionTool, ObjectDetectionTool
+
+openai_api_key = "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"
+
+
+def image_to_text(image=None) -> str:
     """
-    未完成。LangChain による MiniGPT-4 の呼び出しを利用して実装する予定。
+    入力として受け取った画像に対し、画像の雰囲気に合った音楽名を返す関数。
     """
-    return "一途"
+    #initialize the gent
+    tools = [ImageCaptionTool(), ObjectDetectionTool()]
+
+    conversational_memory = ConversationBufferWindowMemory(
+        memory_key='chat_history',
+        k=5,
+        return_messages=True
+    )
+
+    llm = ChatOpenAI(
+        openai_api_key= openai_api_key,
+        temperature=0,
+        model_name="gpt-3.5-turbo"
+    )
+
+    agent = initialize_agent(
+        agent="chat-conversational-react-description",
+        tools=tools,
+        llm=llm,
+        max_iterations=5,
+        verbose=True,
+        memory=conversational_memory,
+        early_stopping_method='generate'
+    )
+
+    # Prompt
+    image_path = "/Users/naoki/github/Rutilea/img/suits_dining_scene.jpg"
+    # user_question = "generate a caption for this iamge?"
+    user_question = "Tell me the atmosphere of this image."
+    response = agent.run(f'{user_question}, this is the image path: {image_path}')
+    print(response)
+
+    return response
+
+
+if __name__ == "__main__":
+
+    image_to_text()
