378bda953549785ea4ff39aa873f45531e76e327
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2023-10-22 15:41:27 +0900

    LLM output as list format

--- src/spotify.py
+++ src/spotify.py
@@ -13,29 +13,34 @@ def search_spotify(name: str) -> tuple:
 
     results = spotify.search(q='track:'+name, type='track')
 
-    # Artwork, name of track, name of artist
-    artwork_url = results["tracks"]["items"][0]["album"]["images"][0]["url"]    # Usually, 'images' have more than 1 artwork.
-
-    track_name = results["tracks"]["items"][0]["album"]["name"]
-    track_URL = results["tracks"]["items"][0]["album"]["external_urls"]["spotify"]
-
-    artist_name = results["tracks"]["items"][0]["album"]["artists"][0]["name"]
-    artist_URL = results["tracks"]["items"][0]["album"]["artists"][0]["external_urls"]["spotify"]
-
-    result = {
-        "artwork_url": artwork_url,
-        "track_name": track_name,
-        "track_url": track_URL,
-        "artist_name": artist_name,
-        "artist_url": artist_URL
-        }
+    if len(results["tracks"]["items"]) == 0:
+        print("No tracks found.")
+        return None
     
-    print(result)
+    else:
+        # Artwork, name of track, name of artist
+        artwork_url = results["tracks"]["items"][0]["album"]["images"][0]["url"]    # Usually, 'images' have more than 1 artwork.
 
-    return result
+        track_name = results["tracks"]["items"][0]["album"]["name"]
+        track_URL = results["tracks"]["items"][0]["album"]["external_urls"]["spotify"]
+
+        artist_name = results["tracks"]["items"][0]["album"]["artists"][0]["name"]
+        artist_URL = results["tracks"]["items"][0]["album"]["artists"][0]["external_urls"]["spotify"]
+
+        result = {
+            "artwork_url": artwork_url,
+            "track_name": track_name,
+            "track_url": track_URL,
+            "artist_name": artist_name,
+            "artist_url": artist_URL
+            }
+        
+        print(result)
+
+        return result
 
 
 if __name__ == "__main__":
 
-    info = search_spotify("Kaiserwalzer")
+    info = search_spotify("Romantic Dinner")
     print(info)
--- src/visual_LLM.py
+++ src/visual_LLM.py
@@ -1,19 +1,19 @@
-import os
-import openai
+# import os
+# import openai
 from tempfile import NamedTemporaryFile
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from image_tools import ImageCaptionTool, ObjectDetectionTool
 
-openai_api_key = "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"
+from langchain.output_parsers import CommaSeparatedListOutputParser, NumberedListOutputParser
 
 
 def image_to_text(image=None) -> str:
     """
     入力として受け取った画像に対し、画像の雰囲気に合った音楽名を返す関数。
     """
-    #initialize the gent
+    #initialize the agent
     tools = [ImageCaptionTool(), ObjectDetectionTool()]
 
     conversational_memory = ConversationBufferWindowMemory(
@@ -23,7 +23,7 @@ def image_to_text(image=None) -> str:
     )
 
     llm = ChatOpenAI(
-        openai_api_key= openai_api_key,
+        openai_api_key="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",
         temperature=0,
         model_name="gpt-3.5-turbo"
     )
@@ -40,14 +40,38 @@ def image_to_text(image=None) -> str:
 
     # Prompt
     image_path = "/Users/naoki/github/Rutilea/img/suits_dining_scene.jpg"
-    # user_question = "generate a caption for this iamge?"
-    user_question = "Tell me the atmosphere of this image."
+    user_question = """Please suggest some names of music pieces which fit to this image."""
+    
     response = agent.run(f'{user_question}, this is the image path: {image_path}')
+
+    print(user_question)
     print(response)
 
     return response
 
 
+def parser(response: str) -> list:
+
+    # output_parser = CommaSeparatedListOutputParser()
+    output_parser = NumberedListOutputParser()
+    result = output_parser.parse(response)
+    return result
+
+
 if __name__ == "__main__":
 
-    image_to_text()
+    # response = image_to_text()
+    response = """
+Based on the image description, some music pieces that could fit the scene are: 
+
+1. Romantic Dinner by Kevin MacLeod
+2. Jazz Restaurant by Music For Video
+3. Elegant Dinner Jazz by Background Music For Video
+4. Fine Dining by Music For Video
+5. Smooth Jazz Dinner Party by Background Music For Video
+
+These are just a few suggestions, and the choice of music ultimately depends on the mood and atmosphere you want to create. Enjoy your music selection!
+"""
+
+    result = parser(response)
+    print(result)
